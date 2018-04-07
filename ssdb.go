9818d82ccd65f51e49c731b42b1b3c0ab83c4252
ego008 <ego008@gmail.com>
2018-04-07 13:54:58 +0700

    Update ssdb.go

--- ssdb.go
+++ ssdb.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package ssdbgo // import "github.com/lynkdb/ssdbgo"
+package ssdbgo
 
 import (
 	"fmt"
